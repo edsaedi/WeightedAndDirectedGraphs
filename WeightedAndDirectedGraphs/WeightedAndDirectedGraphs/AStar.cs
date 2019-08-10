90eda9165af5fea63c4346b420ec2ed22aa5f3b3
Edan Saedi <edsaedi@gmail.com>
2019-08-09 17:29:34 -0700

    I finished the A*

--- WeightedAndDirectedGraphs/WeightedAndDirectedGraphs/AStar.cs
+++ WeightedAndDirectedGraphs/WeightedAndDirectedGraphs/AStar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
+using System.Linq;
 
 namespace WeightedAndDirectedGraphs
 {
@@ -36,8 +37,57 @@ namespace WeightedAndDirectedGraphs
             }
 
             info[start] = (null, 0, HeuristicsFunc(heuristic, start.Value, end.Value));
+            queu.Enqueu(start);
 
-            return null;
+            while (queu.Count > 0)
+            {
+                var current = queu.Deqeue();
+                current.IsVisited = false;
+
+                foreach (var edge in current.Neighbors)
+                {
+                    var neighbor = edge.EndingPoint;
+                    float tentative = edge.Distance + info[start].distance;
+
+                    if (tentative < info[neighbor].distance)
+                    {
+                        neighbor.IsVisited = false;
+                        info[neighbor] = (current, tentative, HeuristicsFunc(heuristic, current.Value, neighbor.Value));
+                    }
+
+                    if (!queu.Contains(neighbor) && !neighbor.IsVisited)
+                    {
+                        queu.Enqueu(neighbor);
+                    }
+                }
+
+                if (current == end)
+                {
+                    break;
+                }
+            }
+
+            var path = new Stack<Vertex<Point>>();
+
+            var currentV = info.Where(x => x.Key == end).First();
+
+            while(true)
+            {
+                path.Push(currentV.Key);
+                
+                if(currentV.Value.founder == null)
+                {
+                    if(path.Count == 1)
+                    {
+                        return null;
+                    }
+                    break;
+                }
+
+                currentV = new KeyValuePair<Vertex<Point>, (Vertex<Point> founder, float distance, float finalDistance)>();
+            }
+
+            return path;
         }
 
         public static float HeuristicsFunc(Heuristics heuristics, Point start, Point end, float D = 1)
